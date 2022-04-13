4502f050bf455353d9ab6a3af0420031bef99bbe
Amandine Sismondi <am.sismondi@gmail.com>
2022-04-13 12:11:33 +0200

    post header style added

--- front/src/components/Home/RecentPosts.vue
+++ front/src/components/Home/RecentPosts.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="BlockPosts" v-for="postItem in posts" :key="postItem.postId">
-         <p class="BlockPosts__content">{{postItem.content}}</p>
-        <img class="BlockPosts__image" :src="postItem.imageUrl" alt="photo de profil">
-
-
-
+        <div class="BlockPosts__user">
+            <img class="BlockPosts__user__picture" src="@/assets/images/profilePicDefault.jpg" alt="">
+            <div class="BlockPosts__user__name">
+                <h2>{{postItem.userId}}</h2>
+                <p>{{postItem.created}}</p>
+            </div>
+        </div>
+        <div class="BlockPosts__content">
+            <p>{{postItem.content}}</p>
+        </div>
+        <img class="BlockPosts__image" :src="postItem.imageUrl" alt="post photo">
     </div>
 </template>
 
@@ -13,7 +19,7 @@
 import { mapGetters, mapState } from 'vuex';
 export default ({
     name: 'RecentPosts',
-   mounted:
+    mounted:
         function(){
             this.$store
                 .dispatch('getPostsByDate')
@@ -22,13 +28,12 @@ export default ({
             });
             
         },
-    
     computed: {
         ...mapState({
             posts: 'posts'
         }),
         ...mapGetters(['fullname'])
-  },
+    }
 })
 </script>
 
@@ -38,11 +43,27 @@ export default ({
         background-color: white;
         margin: 30px auto;
         width: 500px;
-        //height: 100px;
+       // height: 100px;
         align-items: center;
         border-radius: 20px 20px 0 0;
       /* position: absolute;
         z-index: 3;*/
+        &__user{
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+            &__picture{
+                width: 90px;
+                height: 90px;
+                margin: 5px;
+                border-radius: 50px;
+                border: 2px solid #999999;
+            }
+            &__name{
+                margin: 20px 0;
+                text-align: left;;
+            }
+        }
         &__content{
             margin: 20px 10px;
         }
